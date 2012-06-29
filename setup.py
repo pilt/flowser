8d415075af1d7320355156b2e9a71b186d7e6d71
Simon Pantzare <simon@pewpewlabs.com>
2012-06-29 11:39:31 +0200

    Add license and copyright info; bump version

--- setup.py
+++ setup.py
@@ -1,7 +1,7 @@
 from setuptools import setup
 
 setup(name="flowser",
-      version="0.1.0",
+      version="0.1.1",
       description="High-level interface for Amazon Simple Workflow",
       author="Simon Pantzare",
       author_email="simon+flowser@pewpewlabs.com",
